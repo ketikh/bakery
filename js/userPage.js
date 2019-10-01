6576630898879fcf701ad74de07fbe1a862689dd
Your Name <keti.khukhia@wandio.com>
2019-10-01 15:20:19 +0400

    add remove button

--- js/userPage.js
+++ js/userPage.js
@@ -1,29 +1,50 @@
-document.getElementById("addProduct").addEventListener("click", addProduct);
+document.getElementById("addproduct").addEventListener("click", addProduct);
 
 let userName;
 if (localStorage.getItem("_Auth")) {
   userName = JSON.parse(localStorage.getItem("_Auth")).name;
 }
 
+function removeProduct(a) {
+  a.parentNode.remove();
+  let getKey = "_" + userName + " " + a.parentNode.children[0].textContent;
+  localStorage.removeItem(getKey);
+}
+
 function addProduct() {
   let productName = prompt("Please enter product name");
-  let productCalorie = prompt("Please enter product Calorie");
-  let container = document.getElementById("product-blocks");
-  let div = document.createElement("div");
-  div.classList.add("prod-block");
-  div.innerHTML =
-    "<h1>" +
-    productName +
-    "</h1> <div>" +
-    productCalorie +
-    "</div> <button type='button' class='remove-product'>remove</button>";
-  container.appendChild(div);
-  let obj = {
-    name: productName,
-    calorie: productCalorie
-  };
+  if (productName == "" || productName == null || productName == false) {
+    alert("Name is empty, try again");
+  } else {
+    let productCalorie = prompt("Please enter product Calorie");
+    if (
+      productCalorie == "" ||
+      productCalorie == null ||
+      productCalorie == false
+    ) {
+      alert("Calorie is empty, try again");
+    } else {
+      let container = document.getElementById("product-blocks");
+      let div = document.createElement("div");
+      div.classList.add("prod-block");
+      div.innerHTML =
+        "<label>Product Name: </label><h1>" +
+        productName +
+        "</h1> <div><label>Product Calories: </label>" +
+        productCalorie +
+        "</div> <button type='button' class='remove-product' onclick='removeProduct(this)'><img src='/images/close.png'></button>";
+      container.appendChild(div);
+      let obj = {
+        name: productName,
+        calorie: productCalorie
+      };
 
-  localStorage.setItem("_" + userName + " " + productName, JSON.stringify(obj));
+      localStorage.setItem(
+        "_" + userName + " " + productName,
+        JSON.stringify(obj)
+      );
+    }
+  }
 }
 
 let count = 0;
@@ -35,23 +56,17 @@ for (var key in localStorage) {
     div.classList.add("prod-block");
     container.appendChild(div);
     div.innerHTML =
-      "<h1>" +
+      "<label>Product Name: </label><h1>" +
       JSON.parse(localStorage.getItem(key)).name +
-      "</h1> <div>" +
+      "</h1> <div><label>Product Calories: </label>" +
       JSON.parse(localStorage.getItem(key)).calorie +
-      "</div> <button type='button' class='remove-product'>remove</button>";
+      "</div> <button type='button' class='remove-product' onclick='removeProduct(this)'><img src='/images/close.png'></button>";
     container.appendChild(div);
     count++;
   }
 }
+//let cbox = document.querySelectorAll(".remove-product");
 
-function removeProduct() {
-  let getKey = "_" + userName + " " + this.parentNode.children[0].textContent;
-  localStorage.removeItem(getKey);
-}
-
-let cbox = document.querySelectorAll(".remove-product");
-
-for (let i = 0; i < cbox.length; i++) {
-  cbox[i].addEventListener("click", removeProduct);
-}
+// for (let i = 0; i < cbox.length; i++) {
+//   cbox[i].addEventListener("click", removeProduct);
+// }
